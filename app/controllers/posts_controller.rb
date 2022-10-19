1d3009891cd0a6e093fc9bbaeb54853798211e91
JHorger <Joshua_Horger@reyrey.com>
2022-10-19 17:07:02 -0500

    stuck on some things

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,27 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = current_user.posts.build
   end
 
   def create
+    @post = current_user.posts.build(post_params)
+    
+    if @post.save
+      redirect_to root_path, notice: "Post was successfully created!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:post)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root to: "posts#index"
+
+  resources :posts
 end
